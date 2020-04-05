91cceb2fa0ca92c064821ca3120834eb4ce70314
sudheervk88 <sudheervk88@gmail.com>
2020-04-05 21:41:11 +0530

    button helper modified

--- src/main/java/helperFunctions/ButtonHelper.java
+++ src/main/java/helperFunctions/ButtonHelper.java
@@ -1,7 +1,14 @@
 package helperFunctions;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ButtonHelper {
    private static ButtonHelper buttonHelper;
@@ -21,10 +28,58 @@ public class ButtonHelper {
    }
 
    public void click(By locator){
-
+	   
+     WebDriverWait wait = getWait();
+     wait.until(ExpectedConditions.elementToBeClickable(locator));
+	 WebElement element = webDriver.findElement(locator);
+	// flashElement(element, webDriver);  
+	 highLighterMethod(webDriver, element);
      webDriver.findElement(locator).click();
 
     }
 
+   
+    public void flashElement(WebElement element,WebDriver driver) {
+    	
+    	String bgColor = element.getCssValue("backgroundColor");
+    	
+    	for(int i=0;i<5;i++) {
+    		changeColor("rgb(255,0,0)",element,driver);
+    		changeColor(bgColor, element, driver);
+    	}
+    	
+    }
+    
+    
+    public void changeColor(String color,WebElement element,WebDriver driver) {
+    	
+    	JavascriptExecutor js = (JavascriptExecutor) driver;
+    	js.executeScript("arguments[0].style.backgroundColor = '"+color+"'",element);
+    	
+    	try {
+    		Thread.sleep(20);
+			
+		} catch (InterruptedException e) {
+			
+			e.printStackTrace();
+		}
+    }
+    
+    
+    public void highLighterMethod(WebDriver driver, WebElement element){
+    	JavascriptExecutor js = (JavascriptExecutor) driver;
+    	String bgColor = element.getCssValue("backgroundColor");
+        js.executeScript("arguments[0].setAttribute('style', 'background: "+bgColor+"; border: 2px solid red;');", element);
+        
+        }
+   
+    @SuppressWarnings("deprecation")
+	private WebDriverWait getWait(){
+        WebDriverWait wait = new WebDriverWait(webDriver,60);
+        wait.pollingEvery(250, TimeUnit.MILLISECONDS);
+        wait.ignoring(NoAlertPresentException.class);
+        return wait;
+    }
+   
 
 }
